04badc39843bb4b29ac95131ea347935cfbd3b55
Kishore Babu <barykaed@gmail.com>
2020-02-09 11:38:35 +0530

    Add: Empty State for notes list added

---
+++ app/src/main/java/com/kishorebabu/android/notes/ui/noteslist/NotesListActivity.kt
@@ -0,0 +1,12 @@
+package com.kishorebabu.android.notes.ui.noteslist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kishorebabu.android.notes.R
+
+class NotesListActivity : AppCompatActivity(R.layout.activity_notes_list) {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}

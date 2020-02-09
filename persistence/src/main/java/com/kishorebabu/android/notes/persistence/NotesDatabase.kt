0aad6b487708a383496c25ec9471bbf4f11a8849
Kishore Babu <barykaed@gmail.com>
2020-02-10 00:39:35 +0530

    Fix: Missing Database annotation

--- persistence/src/main/java/com/kishorebabu/android/notes/persistence/NotesDatabase.kt
+++ persistence/src/main/java/com/kishorebabu/android/notes/persistence/NotesDatabase.kt
@@ -1,10 +1,16 @@
 package com.kishorebabu.android.notes.persistence
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.kishorebabu.android.notes.persistence.dao.NoteDao
+import com.kishorebabu.android.notes.persistence.model.Note
 
+@Database(
+    entities = [Note::class],
+    version = NotesDatabase.VERSION
+)
 abstract class NotesDatabase : RoomDatabase() {
   companion object {
     const val VERSION = 1
